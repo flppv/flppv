4102f3db6b47b9d64b281407952c7b8a88b81a12
Anton Filippov <flppv8@gmail.com>
2016-04-19 12:14:17 +0700

    change faq page, add hash links

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 $(function(){
+
+  if (window.location.hash) {
+    var hash = window.location.hash; 
+    $(hash).find(".answer").stop().slideToggle(60);
+  }
+
   $(".question h2").click(function(){
     $(this).parent().find(".answer").stop().slideToggle(60);
   });
-  var now = (new Date()).getTime(),
-      boomDate = (new Date(2016, 3, 6, 17)).getTime(),
-      diff = boomDate - now,
-      DAY = 24*60*60*1000,
-      leftDays = Math.round(diff/DAY),
-      word;
-  switch(leftDays) {
-    case 6: 
-      word = "дней";
-      break;
-    case 4: 
-      word = "дня";
-      break;
-    case 1: 
-      word = "день";
-      break;
-    default: 
-      word = "дней";
-  }
-  $("#left").html( leftDays + " " + word);
 });
