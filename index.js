20c5585fb1035bf9d23fcd1bd23f430c4736bab3
Anton Filippov <flppv8@gmail.com>
2016-05-11 21:01:33 +0700

    add animating numbers

---
+++ index.js
@@ -0,0 +1,12 @@
+$(init);
+function init() {
+  $("#code").animateNumber({
+    number: 46
+  }, 1500);
+  $("#pos").animateNumber({
+    number: 114
+  }, 2000);
+  $("#gh").animateNumber({
+    number: 350
+  }, 1750);
+}
